780059e194c6f50fb5a597b286442b43e1088652
Ashkaan Jaberi <ashkaan14@gmail.com>
2017-09-09 14:34:34 -0700

    form working

--- public/scripts/view/newToilets.js
+++ public/scripts/view/newToilets.js
@@ -4,4 +4,31 @@ var app = app || {};
 
 (function(module){
 
+const newToilet = {};
+
+newToilet.handleForm = function(){
+  $('#new-toilet').on('submit', newToilet.submit);
+};
+
+newToilet.submit = function(event) {
+  event.preventDefault();
+  let toilet = {
+    location: $('#location').val(),
+    overallQuality: $('input[name=toilet]:checked', '#new-toilet').val(),
+    tpQuality: $('input[name=tp]:checked', '#new-toilet').val(),
+    usage: $('input[name=use]:checked', '#new-toilet').val(),
+    occupancy: $('input[name=occupancy]:checked', '#new-toilet').val(),
+    genderNeutral: $('input[name=gender]:checked', '#new-toilet').val(),
+    soap: $('input[name=soap]:checked', '#new-toilet').val(),
+    drying: $('input[name=drying]:checked', '#new-toilet').val(),
+    comments: $('#comments').val(),
+  }
+  console.log(toilet);
+}
+
+
+  newToilet.handleForm();
+  module.newToilet = newToilet;
+
+
 })(app);
