7096db2436daf9149c62d64a0995cf0ef4e0ecf4
Dalton Carr <daltonlcarr@yahoo.com>
2017-09-09 10:59:10 -0700

    map running

--- public/scripts/view/map.js
+++ public/scripts/view/map.js
@@ -1,4 +1,5 @@
 'use strict';
+console.log('running');
 function initMap() {
   var uluru = {lat: -25.363, lng: 131.044};
   var map = new google.maps.Map(document.getElementById('map'), {
--- server.js
+++ server.js
@@ -3,8 +3,8 @@
 const pg = require('pg');
 const fs = require('fs');
 const express = require('express');
-const bodyParser = require('body-parser');
-const requestProxy = require('express-request-proxy');
+// const bodyParser = require('body-parser');
+// const requestProxy = require('express-request-proxy');
 const PORT = process.env.PORT || 3000;
 const app = express();
 
@@ -13,7 +13,7 @@ const client = new pg.Client(conString);
 client.connect();
 client.on('error', err => console.log(err));
 
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
 app.use(express.static("./public"));
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}!`));
