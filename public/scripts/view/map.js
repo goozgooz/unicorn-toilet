45316da8f35022425e81223cc58746eba0692540
Dalton Carr <daltonlcarr@yahoo.com>
2017-09-14 20:53:13 -0700

    info window is working on first marker

--- public/scripts/view/map.js
+++ public/scripts/view/map.js
@@ -67,36 +67,14 @@ function initMap() {
   var contentString = '<div id="content">'+
      '<div id="siteNotice">'+
      '</div>'+
-     '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
-     '<div id="bodyContent">'+
-     '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
-     'sandstone rock formation in the southern part of the '+
-     'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
-     'south west of the nearest large town, Alice Springs; 450&#160;km '+
-     '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
-     'features of the Uluru - Kata Tjuta National Park. Uluru is '+
-     'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
-     'Aboriginal people of the area. It has many springs, waterholes, '+
-     'rock caves and ancient paintings. Uluru is listed as a World '+
-     'Heritage Site.</p>'+
-     '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
-     'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
-     '(last visited June 22, 2009).</p>'+
-     '</div>'+
+     '<h1 id="firstHeading" class="firstHeading">Toilet!</h1>'+
      '</div>';
 
   var infowindow = new google.maps.InfoWindow({
   content: contentString
 });
-  marker.addListener('click', function() {
-  infowindow.open(map, marker);
-  });
-}
 
-
-
-
-window.eqfeed_callback = function(results) {
+window.toiletsJson = function(results) {
   for (var i = 0; i < results.features.length; i++) {
     var coords = results.features[i].geometry.coordinates;
     var latLng = new google.maps.LatLng(coords[1],coords[0]);
@@ -104,15 +82,23 @@ window.eqfeed_callback = function(results) {
       position: latLng,
       map: map
     });
+    var infowindow = new google.maps.InfoWindow({
+      content: contentString
+    });
   }
 }
 
-function loadMarkers(){
-  app.Toilet.all.forEach(toilet => toilet.info());
-  app.Toilet.all.forEach(toilet => toilet.geocode());
-  app.Toilet.all.forEach(toilet => addMarker(toilet));
-}
+// function loadMarkers(){
+//   app.Toilet.all.forEach(toilet => toilet.info());
+//   app.Toilet.all.forEach(toilet => toilet.geocode());
+//   app.Toilet.all.forEach(toilet => addMarker(toilet));
+// }
 
-function initIndexPage(){
-  app.Toilet.fetchData(loadMarkers);
+// function initIndexPage(){
+//   app.Toilet.fetchData(loadMarkers);
+// }
+  marker.addListener('click', function() {
+    console.log('click event');
+  infowindow.open(map, marker);
+  });
 }
